be8338b72fbe321d6ca458c00ec77e2c282e0326
Sehannnnnnn <stanley710@naver.com>
2023-12-04 11:50:13 +0900

    main

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var port = 8080;
 http.createServer( (req, res) => {
   console.log('Requested: ' + req.url);
   res.writeHead(200, { 'Content-Type':'text/html; charset=utf-8' });
-  res.write('<h1>docker 예제1</h1>');
+  res.write('<h1>docker 예제 메인</h1>');
   res.write('<h2>Hello node!</h2>');
   res.end();
 }).listen(port, () => {
